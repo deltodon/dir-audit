b73f2c4a23ad198e21bcc42744755f1a05219677
Jiri Klic <web@jiriklic.com>
2024-10-18 21:17:30 +0100

    Add check functions

--- dir_audit/parser.py
+++ dir_audit/parser.py
@@ -18,7 +18,8 @@ def get_args():
     parser_make.add_argument("path", help="Directory destination")
 
     parser_check.add_argument("path", help="Directory path")
-    parser_check.add_argument("-c", "--contains")
-    parser_check.add_argument("-e", "--empty")
+    parser_check.add_argument("-c", "--contains", help="List files and directories that contain given substring")
+    parser_check.add_argument("-e", "--empty", action="store_true", default=False, help="Check if directory is empty")
+    parser_check.add_argument("-f", "--full", action="store_true", default=False, help="Print full directory tree")
 
     return parser.parse_args()
